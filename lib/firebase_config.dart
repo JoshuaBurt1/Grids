60b67573b7c739de23ebb9f1079d5d07868d8ebc
Joshua Burt <Joshua.Burt@MyGeorgian.ca>
2026-02-27 14:49:05 -0500

    Database: changed.

--- lib/firebase_config.dart
+++ lib/firebase_config.dart
@@ -2,13 +2,13 @@ import 'package:firebase_core/firebase_core.dart';
 
 class FirebaseConfig {
   static const FirebaseOptions currentPlatform = FirebaseOptions(
-    apiKey: "AIzaSyBM3XixppIif9vvd8rwR8mML4xaTLf2JrQ",
-    authDomain: "squarehexagon-games.firebaseapp.com",
-    projectId: "squarehexagon-games",
-    storageBucket: "squarehexagon-games.firebasestorage.app",
-    messagingSenderId: "323592971309",
-    appId: "1:323592971309:web:ca296a8f4eca5f8b8cc4a4",
-    measurementId: "G-788TSS1VVF",
+    apiKey: "AIzaSyB1jhXYM_1nkOpvkhokcik9_zYSrSenRRM",
+    authDomain: "squarehexagon-holdings.firebaseapp.com",
+    projectId: "squarehexagon-holdings",
+    storageBucket: "squarehexagon-holdings.firebasestorage.app",
+    messagingSenderId: "702841156351",
+    appId: "1:702841156351:web:b105027698de92b56d52ca",
+    measurementId: "G-QV7Q6LZZHJ"
   );
 
   static Future<void> init() async {
